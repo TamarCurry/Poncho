286265cc37123d5f053ca847856dd2c9f1184f1f
Tamar Curry <tcurry@playtika.com>
2016-06-23 11:34:58 -0700

    Member changes
    * Sprite imageWidth and imageHeight are now ushorts
    * TextField extends DisplayObject instead of Sprite now, has its own
    unique pivot settings

--- Display/Sprite.cs
+++ Display/Sprite.cs
@@ -4,8 +4,8 @@ namespace Poncho.Display
 {
 	public class Sprite : DisplayObjectContainer
 	{
-		public int imageWidth { get { return image?.rect.width ?? 0; } }
-		public int imageHeight { get { return image?.rect.height ?? 0; } }
+		public ushort imageWidth { get { return image?.rect.width ?? 0; } }
+		public ushort imageHeight { get { return image?.rect.height ?? 0; } }
 		public ushort pivotX { get { return image?.pivot.x ?? 0; } }
 		public ushort pivotY { get { return image?.pivot.y ?? 0; } }
 		public Image image;
--- Text/TextField.cs
+++ Text/TextField.cs
@@ -2,29 +2,17 @@ using Poncho.Display;
 
 namespace Poncho.Text
 {
-	public class TextField : Sprite
+	public class TextField : DisplayObject
 	{
-		//private ushort _pivotX;
-		//private ushort _pivotY;
-
-		public ushort width;
-		public ushort height;
 		public bool wordWrap;
 		public bool multiline;
 		public bool clipOverflow;
+		public float pivotX;
+		public float pivotY;
 		public string text;
+		public ushort width;
+		public ushort height;
 		public TextFormat format;
 		public TextAlignment alignment;
-
-		//public override ushort pivotX { get { return _pivotX; } set { _pivotX = value; } }
-		//public override ushort pivotY { get { return _pivotY; } set { _pivotY = value; } }
-
-		public TextField()
-		{
-			//_pivotX = 0;
-			//_pivotY = 0;
-			width	= 0;
-			height	= 0;
-		}
 	}
 }
